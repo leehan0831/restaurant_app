5fc4844b8c463844c998ce70c6bbac345177bfd0
Yeehan <leeyeehan08@gmail.com>
2025-05-08 15:47:16 +0800

    Add Flutter project files

---
+++ android/app/src/main/kotlin/com/dbestech/restaurant_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dbestech.restaurant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/views/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Home")));
+  }
+}
